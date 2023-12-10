d8e0fe90a5c77b4e6b0df40819e45a36448d2c9c
yoswos <151448407+yoswosyotelc@users.noreply.github.com>
2023-12-10 15:36:47 +0300

    Delete Intro/spoj.py

--- Intro/spoj.py
+++
@@ -1,7 +0,0 @@
-# # @#$!@#$@!#$ hhhh### << ///Sep_2023..??>> @ @#$@%#!@%!Saving??@!#$@!#$@!#$!@#$@!#$@
-# # !!!!! don't stay at one part for more than 1 weekds...!!!!
-# # !!!!! proceed as you go through by commenting .. for furhter refirinement..
-
-
-# # https://www.codechef.com/
-
